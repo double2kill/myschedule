cd8b13c8826bc4f51d1c08ed28776b2e29e703ed
double2kill <379563000@qq.com>
2018-10-16 21:22:12 +0800

    edit schedule

---
+++ jobs/weather/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    users: [
+        '刘晨<379563000@qq.com>',
+        '六七瓶<253184527@qq.com>'
+    ]
+}
--- schedule.js
+++ schedule.js
@@ -3,8 +3,13 @@ const logger = require('./logger')
 const weather = require('./jobs/weather')
 
 function scheduleCronstyle() {
-    schedule.scheduleJob('0 0 9 * * *', function () {
+    schedule.scheduleJob('0 40 21 * * *', () => {
+        logger.info('start get weather')
+        // 这个函数不可以写async,因为超过30s处理时间会抛出错误
         weather()
+            .then(data => {
+                logger.info('end')
+            })
     });
 }
 
