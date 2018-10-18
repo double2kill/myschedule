e8eafe8fafbcd675dc80fbd9e346c7890f6e9811
double2kill <379563000@qq.com>
2018-10-18 11:18:35 +0800

    docker config

--- jobs/weather/index.js
+++ jobs/weather/index.js
@@ -6,7 +6,7 @@ const {
 const moment = require('moment');
 
 const getWeather = async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
     const page = await browser.newPage();
     await page.goto('http://www.weather.com.cn/weather/101230811.shtml');
 
--- schedule.js
+++ schedule.js
@@ -1,9 +1,9 @@
 var schedule = require('node-schedule');
-const logger = require('./logger')
-const weather = require('./jobs/weather')
+const logger = require('./logger');
+const weather = require('./jobs/weather');
 
-function scheduleCronstyle() {
-    schedule.scheduleJob('0 40 21 * * *', () => {
+const scheduleCronstyle = () => {
+    schedule.scheduleJob('0 0 9 * * *', () => {
         logger.info('start get weather')
         // 这个函数不可以写async,因为超过30s处理时间会抛出错误
         weather()
